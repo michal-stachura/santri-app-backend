f46752f854016c2e9883271d8254d7706b621b15
Michal Stachura <michal.stachura@santri.pl>
2023-06-01 13:21:26 +0200

    Add user token authentication

--- santri_app/chat/consumers.py
+++ santri_app/chat/consumers.py
@@ -1,19 +1,42 @@
 import json
+from urllib.parse import parse_qs
 
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.contrib.auth.models import AnonymousUser
+from rest_framework.authtoken.models import Token
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
+    @database_sync_to_async
+    def __get_user(self, token):
+        try:
+            user_token = Token.objects.get(key=token)
+            return user_token.user
+        except Token.DoesNotExist:
+            return AnonymousUser()
+
     async def connect(self):
+        query_string = self.scope["query_string"].decode()
+        params = parse_qs(query_string)
+        token = params.get("token")
+
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
 
-        # Join room group
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-        # Here we can check user privileges to connection.
-        # User = scope["user"]
-        # If user is not allowed you must ommit self.accept()
-        await self.accept()
+        if not token:
+            await self.close()
+        else:
+            token = token[0]
+            user = await self.__get_user(token)
+
+            if user.is_authenticated:
+                self.scope["user"] = user
+                await self.accept()
+                # Join room group
+                await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+            else:
+                await self.close()
 
     async def disconnect(self, close_code):
         # Leave room group
