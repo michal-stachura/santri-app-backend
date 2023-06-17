3989a14c83a986cea2fb08dbf2877227859d2fa7
Michal Stachura <michal.stachura@santri.pl>
2023-06-17 20:29:28 +0200

    Add chat webnsocket urls

--- config/asgi.py
+++ config/asgi.py
@@ -8,12 +8,11 @@ from django.core.asgi import get_asgi_application
 from santri_app.chat.routing import get_chat_websocket_url_patterns
 from santri_app.dashboard.routing import get_dashboard_websocket_urlpatterns
 
-websocket_urlpatterns = get_dashboard_websocket_urlpatterns() + get_chat_websocket_url_patterns()
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
+websocket_urlpatterns = get_dashboard_websocket_urlpatterns() + get_chat_websocket_url_patterns()
 
 application = ProtocolTypeRouter(
     {
