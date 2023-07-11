37e7f2cb1b89eafbd15a6c12b82222f653f2a777
Michal Stachura <michal.stachura@santri.pl>
2023-07-11 22:08:49 +0200

    wip: /me store data

--- santri_app/users/api/serializers.py
+++ santri_app/users/api/serializers.py
@@ -7,8 +7,4 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["name", "url"]
-
-        extra_kwargs = {
-            "url": {"view_name": "api:user-detail", "lookup_field": "pk"},
-        }
+        fields = ["email", "native_language"]
--- santri_app/users/models.py
+++ santri_app/users/models.py
@@ -1,6 +1,6 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField, EmailField
-from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 from santri_app.users.managers import UserManager
@@ -14,22 +14,11 @@ class User(AbstractUser):
     """
 
     # First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
-    first_name = None  # type: ignore
-    last_name = None  # type: ignore
     email = EmailField(_("email address"), unique=True)
-    username = None  # type: ignore
+    native_language = CharField(_("Native language"), max_length=2, default=settings.LANGUAGES[0][0])
+    username = None
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     objects = UserManager()
-
-    def get_absolute_url(self) -> str:
-        """Get URL for user's detail view.
-
-        Returns:
-            str: URL for user detail.
-
-        """
-        return reverse("users:detail", kwargs={"pk": self.id})
